33168e9febf5d12b6f0ff42c2d47b79b671f8698
Kernkraftwerk <kerkraftdev@hotmail.com>
2026-03-06 22:24:49 +0100

    fixed

--- Java-sorting-algorithms/src/main/java/junitTestCaseTest.java
+++ Java-sorting-algorithms/src/main/java/junitTestCaseTest.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class junitTestCaseTest {
     @Test
